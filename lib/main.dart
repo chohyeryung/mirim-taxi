abbafbd4bfe7229a2aadec5ba92b93f9b196493c
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-08-09 15:12:34 +0900

    #4 - 화면이 3개인 UI 작성

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,11 @@ class MyHomePage extends StatefulWidget {
 // 속
 class _MyHomePageState extends State<MyHomePage> {
   var _index=0;
-
+  var _pages=[
+    Page1(),
+    Page2(),
+    Page3(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,12 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
-        child:Text(
-          '$_index 페이지',
-          style: TextStyle(fontSize: 40),
-        ),
-      ),
+      body: _pages[_index],
 
       bottomNavigationBar: BottomNavigationBar(
         onTap:(index){
@@ -74,3 +73,40 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:Text(
+        '홈페이지',
+        style:TextStyle(fontSize:40),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:Text(
+        '이용서비스',
+        style:TextStyle(fontSize:40),
+      ),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:Text(
+        '내 정보',
+        style:TextStyle(fontSize:40),
+      ),
+    );
+  }
+}
+
